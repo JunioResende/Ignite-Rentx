8c94c950944ee544f829b4f2aab023485ad468fe
JunioResende <junioresende23@gmail.com>
2021-04-07 12:19:34 -0300

    integrate entitie user to typeorm

--- src/modules/account/infra/typeorm/entities/User.ts
+++ src/modules/account/infra/typeorm/entities/User.ts
@@ -1,12 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 } from 'uuid';
 
+@Entity('users')
 class User {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @Column()
   driver_license: string;
+
+  @Column()
   isAdmin: boolean;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
