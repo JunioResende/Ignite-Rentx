d0c54c8cef3c33fd2737e413a2ce3793404473d9
JunioResende <junioresende23@gmail.com>
2021-04-07 10:59:49 -0300

    create metods interface repsitory

---
+++ src/modules/account/repositories/ICreateUsersRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
+import { User } from '../infra/typeorm/entities/User';
+
+interface ICreateUsersRepository {
+  create(data: ICreateUserDTO): Promise<void>;
+
+  findByEmail(email: string): Promise<User>;
+
+  findById(id: string): Promise<User>;
+}
+
+export { ICreateUsersRepository };
