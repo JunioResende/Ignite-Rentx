a8f5dfe4cb616a37c360d54ee9955f3b83b88a48
JunioResende <junioresende23@gmail.com>
2021-04-07 09:11:54 -0300

    router configured

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import express from 'express';
+import { createConnection } from 'typeorm';
+
+import { router } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3333, () => {
+  console.log('🚀 Rentx server started on port 3333!...');
+});
