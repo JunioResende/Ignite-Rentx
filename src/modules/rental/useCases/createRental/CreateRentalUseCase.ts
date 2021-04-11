dfef4e4db140344742ea64ccda53abd9ffa41d65
JunioResende <junioresende23@gmail.com>
2021-04-11 10:39:46 -0300

    create rentals and verifications

--- src/modules/rental/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rental/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,7 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-
 import { Rental } from '@modules/rental/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rental/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
-
-dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -14,7 +10,10 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider,
+  ) {}
 
   async execute({
     user_id,
@@ -38,14 +37,12 @@ class CreateRentalUseCase {
       throw new AppError('Theres a rental in progress for user!');
     }
 
-    const expectReturnDateFormat = dayjs(expected_return_date)
-      .utc()
-      .local()
-      .format();
+    const dateNow = this.dateProvider.dateNow();
 
-    const dateNow = dayjs().utc().local().format();
-
-    const compare = dayjs(expectReturnDateFormat).diff(dateNow, 'hours');
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date,
+    );
 
     if (compare < minimumHour) {
       throw new AppError('Invalid return time!');
