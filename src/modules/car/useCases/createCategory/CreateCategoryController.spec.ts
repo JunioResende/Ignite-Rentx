cb93fc3de432f26d0ef2657210b19eb032fa9927
JunioResende <junioresende23@gmail.com>
2021-04-11 12:42:41 -0300

    create test

--- src/modules/car/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/car/useCases/createCategory/CreateCategoryController.spec.ts
@@ -2,8 +2,13 @@ import request from 'supertest';
 
 import { app } from '@shared/infra/http/app';
 
-describe('Create Category Controller', () => {
-  it('test', async () => {
-    await request(app).get('/car/available').expect(201);
+describe('Create Category Controller', async () => {
+  it('should be able to create a new category', async () => {
+    const response = await request(app).post('/categories').send({
+      name: 'Categories Supertest',
+      description: 'Categories Supertest description',
+    });
+
+    expect(response.status).toBe(201);
   });
 });
