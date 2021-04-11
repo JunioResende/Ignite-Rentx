c8b53d8f1077e302c3455f6b003099152bcb9b85
JunioResende <junioresende23@gmail.com>
2021-04-11 00:06:44 -0300

    create new rental

---
+++ src/modules/rental/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,24 @@
+import { RentalsRepositoryInMemory } from '@modules/rental/repositories/in-memory/RentalsRepositoryInMemory';
+
+import { CreateRentalUseCase } from './CreateRentalUseCase';
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe('Create Rental', () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it('should be able to create a new rental', async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: '12345',
+      car_id: '54321',
+      expected_return_date: new Date(),
+    });
+
+    expect(rental).toHaveProperty('id');
+    expect(rental).toHaveProperty('start_date');
+  });
+});
