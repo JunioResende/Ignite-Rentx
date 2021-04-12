ddf6e94d2bcf121c23ee2f4b8278a9b52ce7760c
JunioResende <junioresende23@gmail.com>
2021-04-12 13:45:43 -0300

    useCase

--- src/modules/rental/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rental/infra/typeorm/repositories/RentalsRepository.ts
@@ -37,6 +37,12 @@ class RentalsRepository implements IRentalsRepository {
 
     return rental;
   }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id);
+
+    return rental;
+  }
 }
 
 export { RentalsRepository };
