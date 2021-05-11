bab5a059cf45dc117bc1d089fc16f4f46f2b4a42
JunioResende <junioresende23@gmail.com>
2021-05-11 16:04:21 -0300

    resetPassword

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { ResetPasswordUserController } from '@modules/account/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/account/useCases/sendForgotPasswordMail/sendForgotPasswordMailController';
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRoutes.post('/reset', resetPasswordUserController.handle);
 
 export { passwordRoutes };
