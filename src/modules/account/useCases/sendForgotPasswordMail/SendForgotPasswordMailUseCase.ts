625120ac150fb19e40b3a08e9212d896fedc75f9
JunioResende <junioresende23@gmail.com>
2021-05-18 17:34:54 -0300

    sesProvider

--- src/modules/account/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/account/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -17,7 +17,7 @@ class SendForgotPasswordMailUseCase {
     private usersTokenRepository: IUsersTokensRepository,
     @inject('DayjsDateProvider')
     private dateProvider: IDateProvider,
-    @inject('EtherealMailProvider')
+    @inject('MailProvider')
     private mailProvider: IMailProvider,
   ) {}
 
